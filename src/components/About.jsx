786e9d5afe338d4e803c2fe02b41427a14002f6a
Mruthunjay <idk.mratyunjay@gmail.com>
2024-03-07 09:00:00 +0530

    Marquee

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl">
+      <h1 className="font-['Neue_Montreal'] text-3xl">
+        Ochi is a strategic partner for fast-growing tech businesses that need
+        to raise funds, sell products, explain complex ideas, and hire great
+        people.
+      </h1>
+    </div>
+  );
+}
+
+export default About;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,7 +1,17 @@
+import { motion } from "framer-motion"
 
 function Marquee() {
   return (
-    <div>Marquee</div>
+    <div className="w-full py-20 rounded-3xl bg-[#004D43]">
+
+        <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden gap-10">
+
+            <h1 className="text-[15vw] leading-none font-[Founders_Grotesk_x_condensed] uppercase -mb-20 pt-10">we are osm</h1>
+
+            <h1 className="text-[15vw] leading-none font-[Founders_Grotesk_x_condensed] uppercase -mb-20 pt-10">we are osm</h1>
+
+        </div>
+    </div>
   )
 }
 
